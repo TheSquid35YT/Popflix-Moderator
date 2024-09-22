f07f8ad989e4f8d704c221523c6467c77baa6009
TheSquid35YT <39009874+TheSquid35YT@users.noreply.github.com>
2024-09-22 16:22:25 -0500

    Update unmute.js
    Removed Admin from LolzmanXD (ID:555771957535047700)

--- commands/unmute.js
+++ commands/unmute.js
@@ -24,7 +24,7 @@ module.exports = {
           let member = message.mentions.members.first();
 
           setTimeout(() => {
-            if ((member.id == '344140331941363733')||(member.id == '412278016429785089')||(member.id == '490196736103677975')||(member.id == '479055378991284273')||(member.id == '555771957535047700')/*||(member.id == '274644519591477250')*/||(member.id == '512034141248946188')||(member.id == '460969230439940108')||(member.id == '291373805769850882')||(member.id == '340556433693343756')) {
+            if ((member.id == '344140331941363733')||(member.id == '412278016429785089')||(member.id == '490196736103677975')||(member.id == '479055378991284273')/*||(member.id == '555771957535047700')*//*||(member.id == '274644519591477250')*/||(member.id == '512034141248946188')||(member.id == '460969230439940108')||(member.id == '291373805769850882')||(member.id == '340556433693343756')) {
               member.roles.add(adminRole);
             }
             member.roles.remove(role);
