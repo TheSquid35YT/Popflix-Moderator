092b2d69ace9597e296402ffe6b67abbf274582b
The_Squid_35YT <Knightcraft35@gmail.com>
2025-03-29 23:14:02 -0500

    added temporary JT birthday

--- commands/birthday.js
+++ commands/birthday.js
@@ -63,10 +63,10 @@ module.exports = {
 
     //JT Misevicz (March 30th, 2005)
     //if (d.toLocaleTimeString('en-US', options) === 'March 30, 5:00 AM UTC') {
-    //if (d.toLocaleTimeString('en-US', options) === 'March 30, 2025 at CDT') {
+    if (d.toLocaleTimeString('en-US', options) === 'March 30, 2025 at CDT') {
       //12:00 AM CST is "5:00:00 AM UTC"
-    //  textChannel.send('Hey @everyone, it\'s <@490196736103677975>\'s Birthday! **Happy  Birthday <@490196736103677975>!**');
-    //}
+      textChannel.send('Hey @everyone, it\'s <@490196736103677975>\'s Birthday! **Happy  Birthday <@490196736103677975>!**');
+    }
     //April
     //Meadow Gehrlein (April 1st, 2005)
     if (d.toLocaleTimeString('en-US', options) === 'April 1, 5:00 AM UTC') {
